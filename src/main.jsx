75c39def68f717a99b54ab91d5637400d650e656
Rahul Kumar <rahulkumar703@outlook.com>
2025-07-05 12:33:58 +0530

    added basename

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Collaborative-Whiteboard-Frontend">
       <App />
       <Toaster />
     </BrowserRouter>
