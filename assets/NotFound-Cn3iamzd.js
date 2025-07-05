ae46759d71cea9088728e7c9bd6db0a4f20877ab
Rahul Kumar <rahulkumar703@outlook.com>
2025-07-05 12:10:10 +0530

    Updates

---
+++ assets/NotFound-Cn3iamzd.js
@@ -0,0 +1,6 @@
+import{c as s,u as o,j as e}from"./index-DSr9Ncjg.js";import{B as a}from"./button-CnsQnc8L.js";import"./utils-C1FdZ2tc.js";/**
+ * @license lucide-react v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8",key:"5wwlr5"}],["path",{d:"M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z",key:"1d0kgt"}]],n=s("house",c),x=()=>{const t=o();return e.jsxs("div",{className:"flex flex-col w-full h-screen items-center justify-center gap-6",children:[e.jsx("h1",{className:"text-5xl",children:"404 - Not Found"}),e.jsx("p",{className:"text-center text-lg mt-4",children:"The page you are looking for does not exist."}),e.jsxs(a,{onClick:()=>t("/"),children:[e.jsx(n,{className:"w-4 h-4"}),"Go to Home"]})]})};export{x as default};
